a03bae1170f14f6f77fdcd761b9cfc84eca3ea5a
Hadi Samadzad <h.samadzad@gmail.com>
2022-07-30 18:45:12 +0430

    Add init main nav section to the application

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1 class="mt-8 text-center">Vue App</h1>
+  <main-nav />
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
