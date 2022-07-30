6878059e03e9b4ef380b8347aaa52afbca17ed66
Hadi Samadzad <h.samadzad@gmail.com>
2022-07-30 15:23:18 +0430

    Add open sans font to the project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Vue App</h1>
+  <h1 class="mt-8 text-center">Vue App</h1>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
