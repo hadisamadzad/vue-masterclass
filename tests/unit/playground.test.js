1c80dae7c604d015f3411ecbadd0108fb9790d16
Hadi Samadzad <h.samadzad@gmail.com>
2022-08-07 21:16:03 +0430

    Add playground tests

---
+++ tests/unit/playground.test.js
@@ -0,0 +1,11 @@
+describe("basic math", () => {
+  it("add two numbers", () => {
+    expect(1 + 2).toBe(3);
+    expect(3 + 2).toBe(5);
+  });
+
+  it("multiply two numbers", () => {
+    expect(1 * 2).toBe(2);
+    expect(3 * 2).toBe(6);
+  });
+});
