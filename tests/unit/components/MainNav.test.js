3f6201dfb25f5e29bb408ced457ff286b7ea6098
Hadi Samadzad <h.samadzad@gmail.com>
2022-08-08 14:11:45 +0430

    Remove set data method of mount

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -2,13 +2,9 @@ import { mount } from "@vue/test-utils";
 import MainNav from "@/components/MainNav";
 
 describe("MainNav", () => {
-  it("displays the company name", async () => {
-    const companyName = "My Company Name";
+  it("displays the company name", () => {
+    const companyName = "Bobo Careers";
     const wrapper = mount(MainNav);
-    await wrapper.setData({
-      company: companyName,
-    });
-
     expect(wrapper.text()).toMatch(companyName);
   });
 });
