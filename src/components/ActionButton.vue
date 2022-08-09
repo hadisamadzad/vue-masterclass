6676eb3f07aefec5d5d4abd60ebc6aee82fc5759
Hadi Samadzad <h.samadzad@gmail.com>
2022-08-10 01:46:18 +0430

    Add init sign in action button

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1"
+  >
+    Sign In
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+};
+</script>
