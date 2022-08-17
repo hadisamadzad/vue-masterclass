f485f48a6fe78f856abc03a3fec67f94b6f4dbe9
Hadi Samadzad <h.samadzad@gmail.com>
2022-08-17 12:45:43 +0430

    Add profile image component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imageLink" class="w-8 h-8 object-contain rounded-3xl" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://png.pngitem.com/pimgs/s/78-786293_1240-x-1240-0-avatar-profile-icon-png.png",
+    };
+  },
+};
+</script>
